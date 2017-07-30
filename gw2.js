66f439383b19a7b9f9d5bb91d9ee2c4411ac6635
HunterNL <hunternl@users.noreply.github.com>
2017-07-30 23:00:02 +0200

    add actual (very unfinished) project

---
+++ gw2.js
@@ -0,0 +1,12 @@
+export function createApi(options) {
+  var {apiKey} = options;
+  var endPoint = "https://api.guildwars2.com/v2";
+  
+  function call(resource) {
+    
+    
+    
+  }
+  
+  
+}
